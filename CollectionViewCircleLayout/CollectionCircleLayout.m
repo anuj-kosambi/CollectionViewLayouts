b16d1f4ac5a1ac831684c4ad626e544d33574411
Anuj Kosambi <anujkosambi@gmail.com>
2015-02-23 17:17:49 +0530

    adding layoutAttributes For every Item

--- CollectionViewCircleLayout/CollectionCircleLayout.m
+++ CollectionViewCircleLayout/CollectionCircleLayout.m
@@ -7,12 +7,25 @@
 //
 
 #import "CollectionCircleLayout.h"
-#define kCircleRadius 50
+#define kCircleRadius 35
+
+@interface CollectionCircleLayout () {
+    NSUInteger cellCount;
+    CGPoint centerPoint;
+    NSUInteger radius;
+}
+
+@end
 
 @implementation CollectionCircleLayout
 
 - (void)prepareLayout {
     [super prepareLayout];
+    CGSize size = self.collectionView.frame.size;
+    cellCount = [self.collectionView numberOfItemsInSection:0];
+    centerPoint = CGPointMake(size.width / 2.0, size.height / 2.0);
+    radius = MIN(size.width, size.height) * 0.5  * 0.75;
+    
 }
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
@@ -20,12 +33,19 @@
 }
 
 - (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect {
-    NSArray* attributes = [super layoutAttributesForElementsInRect:rect];
+    NSMutableArray* attributes = [[NSMutableArray alloc] init];
+    for (int i = 0; i < cellCount; i++) {
+        [attributes addObject:[self layoutAttributesForItemAtIndexPath:[NSIndexPath indexPathForItem:i inSection:0]]];
+    }
     return attributes;
 }
 
 - (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath {
-    return [super layoutAttributesForItemAtIndexPath:indexPath];
+    UICollectionViewLayoutAttributes *attributes = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
+    attributes.center = CGPointMake(centerPoint.x + radius * cosf(2 * indexPath.item * M_PI / cellCount),
+                                    centerPoint.y + radius * sinf(2 * indexPath.item * M_PI / cellCount));
+    attributes.size = CGSizeMake(2 * kCircleRadius, 2 * kCircleRadius);
+    return attributes;
 }
 
 @end
