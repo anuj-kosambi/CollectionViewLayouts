117a4771bd1d500470a505d84574f1e7b6915cc4
Anuj Kosambi <anujkosambi@gmail.com>
2015-02-23 14:24:02 +0530

    Initial Commit

---
+++ CollectionViewCircleLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewCircleLayout
+//
+//  Created by AnujKosambi on 23/02/15.
+//  Copyright (c) 2015 AnujKosambi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewCircleLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewCircleLayout
+//
+//  Created by AnujKosambi on 23/02/15.
+//  Copyright (c) 2015 AnujKosambi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
